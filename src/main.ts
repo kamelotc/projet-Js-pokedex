cf10e2469f7761d24ca1cda152100417cc84c232
Colomban Camelot <camelot.colomban2@gmail.com>
2026-01-15 16:28:43 +0100

    Ajout image, et css

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,38 @@
 import './style.css'
 
-interface PokeInfo{
-    id:number;
-    name:string;
+const app = document.querySelector<HTMLDivElement>('#app')
+
+// 1. Correction des backticks et de la syntaxe HTML
+if (app) {
+    app.innerHTML += `
+    <h1>Pokedex</h1>
+    <ul id="pokemon-list"></ul>
+  `
 }
 
-async function getPokemonIndic(id: number) {
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const result = await pokemon.json() as PokeInfo;
-    return result;
+async function getPokemonIndic() {
+    // 2. Correction de l'ID (on cherche bien #pokemon-list créé plus haut)
+    // On précise que c'est un élément de type liste (HTMLUListElement)
+    const liste = document.querySelector<HTMLUListElement>('#pokemon-list')!
+
+    // 3. Correction de l'URL (suppression des guillemets en trop)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1026`)
+    const catalogue = await response.json();
+
+    if (liste) {
+        for (const p of catalogue.results) {
+            const rep = await fetch(p.url)
+            const pokemon = await rep.json()
+
+            // 4. Ajout des backticks pour le template string HTML
+            liste.innerHTML += `
+        <li class="pokemon-card">
+           <span class="pokemon-name">${pokemon.name}</span>
+           <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+        </li>
+      `
+        }
+    }
 }
 
-const app = document.querySelector<HTMLDivElement>('#liste-pokemon')!
-
-for (let i = 1; i < 1026; i++) {
-    const pok = await getPokemonIndic(i);
-    app.insertAdjacentHTML('beforeend',`
-        <div class="pokemon">
-        <p>${i} | ${pok.name}</p>
-        </div>`)
-}
+getPokemonIndic()
