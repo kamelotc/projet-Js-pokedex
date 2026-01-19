43d677d61610528136d15d3ea392c20c5b475e3f
Colomban Camelot <camelot.colomban2@gmail.com>
2026-01-19 17:01:23 +0100

    pagination-ajoue-chiffres

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,8 @@ if (app) {
         <button id="prev-btn">Précédent</button>
         <span id="page-indicator">Page 1</span>
         <button id="next-btn">Suivant</button>
-    </div>
+        <div id="pagination-numbers" class="pagination-numbers"></div>
+
   `
 }
 
@@ -45,26 +46,6 @@ document.querySelector<HTMLInputElement>('#search-input')?.addEventListener('key
     if (e.key === 'Enter') rechercherUnPokemon();
 });
 
-if (app) {
-    app.innerHTML = `
-    <h1>Pokedex</h1>
-    
-    <div class="search-container">
-        <input type="text" id="search-input" placeholder="Chercher un Pokémon (ex: Mewtwo)...">
-        <button id="search-btn">Rechercher</button>
-    </div>
-
-    <ul id="pokemon-list"></ul>
-
-    <div class="pagination-controls">
-        <button id="prev-btn">Précédent</button>
-        
-        <div id="pagination-numbers" class="pagination-numbers"></div>
-        
-        <button id="next-btn">Suivant</button>
-    </div>
-  `
-}
 
 
 rechercherUnPokemon();
